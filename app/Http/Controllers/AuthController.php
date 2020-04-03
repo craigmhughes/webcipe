97915b9865c12d08616f0435507a838bd1071604
Craig Hughes <hghscraig@gmail.com>
2020-04-03 02:49:07 +0100

    Register user through API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,17 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    /**
+     * Attempt login with user credentials. If successful, provide user with a JWT.
+     */
     public function Login(Request $request){
 
+        // Use only the fields needed.
         $credentials = $request->only(["email","password"]);
 
+        // Send error message on failure.
         if(!$token = auth()->attempt($credentials)){
             return response()->json(["error" => "Invalid Credentials", 401]);
         }
 
+        // Send Token
         return response()->json(["token" => $token], 200);
+    }
+
+    /**
+     * Register user, on success return w/ a JWT through Login()
+     */
+    public function Register(Request $request){
+
+        // Use only the fields needed.
+        $credentials = $request->only(["name","email","password","password_confirmation"]);
+
+        // Check provided creds against rules.
+        $validator = Validator::make($credentials, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password'=> ['min:6','required_with:password_confirmation','same:password_confirmation'],
+            'password_confirmation' => ['min:6'],
+        ]);
+
+        // Send error message on failure.
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'error' => $validator->messages()]);
+        }
+
+        // Create new User w/ password hashing.
+        $user = User::create([
+            'name' => $credentials["name"],
+            'email' => $credentials["email"],
+            'password' => Hash::make($credentials["password"])
+        ]);
+
+        // Run Login() to return access token.
+        return $this->login($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post("login", "AuthController@Login");
+    Route::post("register", "AuthController@Register");
 });
 
 Route::resource("recipes", "RecipeController");
