07d71d2b70d0210247c8411d47e7aa7b07c75d1a
Craig Hughes <hghscraig@gmail.com>
2020-04-12 21:00:11 +0100

    Runs register then logs in and checks if user data works

--- test/login.test.js
+++ test/login.test.js
@@ -1,31 +1,115 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const assert = require('assert');
 
+var credentials = [];
+
 /**
- * Authentication Testing
+ * Authentication Testing -- Register User Functionality
  */
-describe('Authentication', function() {
+describe('Authentication', async function() {
   // Set Default test wait time to 0 (Disabled). Starting up the browser will take some time.
   this.timeout(0);
 
-  describe('login()', function() {
+  await describe('register()', function() {
 
-    const tests = [
+    // Generate random email (testXXXX@mail.com)
+    let generateEmail = ()=>{
+      return `test${String(Math.random()).replace("0.","").substr(1,4)}@mail.com`;
+    }
+    
+    // Test Cases to loop through and set params.
+    let tests = [
       {
-        scenario: "incorrect",
-        credentials: ["c@h.com","badPassword"],
-        expectedUrl: "http://localhost:8000/login"
+        scenario: "incorrect password",
+        credentials: ["invalid account", generateEmail(), "password", "badPassword"],
+        expectedUrl: "http://localhost:8000/register"
+      },
+      {
+        scenario: "incorrect email",
+        credentials: ["invalid account", generateEmail().replace("@",""), "password", "password"],
+        expectedUrl: "http://localhost:8000/register"
       },
       {
         scenario: "correct",
-        credentials: ["c@h.com","password"],
+        credentials: ["valid account", generateEmail(), "password", "password"],
+        expectedUrl: "http://localhost:8000/"
+      }
+    ];
+
+    // Assign credentials to var for use in Login()
+    credentials = tests[2].credentials;
+  
+    for(let i = 0; i < tests.length; i++){
+      it(`Register user in with ${tests[i].scenario}`, (done)=>{
+        
+        /**
+         * Carry out manipulation of page (input data) before testing against expected results.
+         *
+         * @returns last completed promise.
+         */
+        async function manipulate() {
+          return (new Promise( async function(resolve, reject) {
+            let driver = await new Builder().forBrowser('chrome').build();
+  
+            async function login(){
+              await driver.get('localhost:8000/register');
+              await driver.findElement(By.name('reg_name')).sendKeys(tests[i].credentials[0]);
+              await driver.findElement(By.name('reg_email')).sendKeys(tests[i].credentials[1]);
+              await driver.findElement(By.name('reg_password')).sendKeys(tests[i].credentials[2]);
+              return await driver.findElement(By.name('reg_password_confirmation')).sendKeys(tests[i].credentials[3], Key.RETURN);
+            }
+            
+            // Run manipulation and wait for page load.
+            login().then(async ()=>{
+              await driver.wait(until.urlIs(tests[i].expectedUrl), 2000).then(()=>resolve(driver)).catch(err=>done(err));        
+            });
+            
+          }));
+        }
+  
+        
+        // Run testing agaist expected results.
+        manipulate().then(async (driver)=>{
+          // Check expected url.
+          assert.equal(await driver.getCurrentUrl(), tests[i].expectedUrl);
+          await driver.quit()
+        }).then(done).catch(err=>done(err));
+        
+      });
+    }
+  });
+});
+
+/**
+ * Authentication Testing -- Login Functionality
+ */
+describe('Authentication', async function() {
+  // Set Default test wait time to 0 (Disabled). Starting up the browser will take some time.
+  this.timeout(0);
+
+  await describe('login()', function() {
+
+    const tests = [
+      {
+        scenario: "incorrect credentials",
+        credentials: [credentials[1],"badPassword"],
+        expectedUrl: "http://localhost:8000/login"
+      },
+      {
+        scenario: "correct credentials",
+        credentials: [credentials[1],credentials[2]],
         expectedUrl: "http://localhost:8000/"
       }
     ];
 
     for(test of tests){
-      it(`Log user in with ${test.scenario} credentials given`, (done)=>{
-      
+      it(`Log user in with ${test.scenario}`, (done)=>{
+        
+        /**
+         * Carry out manipulation of page (input data) before testing against expected results.
+         *
+         * @returns last completed promise.
+         */
         async function manipulate() {
           return (new Promise( async function(resolve, reject) {
             let driver = await new Builder().forBrowser('chrome').build();
@@ -35,7 +119,8 @@ describe('Authentication', function() {
               await driver.findElement(By.name('email')).sendKeys(test.credentials[0]);
               return await driver.findElement(By.name('password')).sendKeys(test.credentials[1], Key.RETURN);
             }
-  
+            
+            // Run manipulation and wait for page load.
             login().then(async ()=>{
               await driver.wait(until.urlIs(test.expectedUrl), 2000).then(()=>resolve(driver));        
             });
@@ -43,8 +128,9 @@ describe('Authentication', function() {
           }));
         }
   
-        
+        // Run testing agaist expected results.
         manipulate().then(async (driver)=>{
+          // Check expected url.
           assert.equal(await driver.getCurrentUrl(), test.expectedUrl);
           await driver.quit()
         }).then(done).catch(err=>done(err));
@@ -52,6 +138,4 @@ describe('Authentication', function() {
       });
     }
   });
-
-  
 });
